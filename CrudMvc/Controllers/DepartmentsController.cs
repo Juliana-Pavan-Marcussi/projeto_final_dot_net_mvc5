ac9aa53ce0a2661c0e81e18690728592eb6a3177
JulianaPavanMarcussi <ju.pavan@yahoo.com.br>
2021-12-09 13:23:49 -0300

    Commit Inicial

---
+++ CrudMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,29 @@
+﻿using CrudMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+
+        public IActionResult Index()
+        {
+
+            List<Department> list = new List<Department>();
+            list.Add(new Department { Id = 1, Name = "Eletrônicos" });
+            list.Add(new Department { Id = 2, Name = "Beleza" });
+
+            return View(list);
+        }
+
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+    }
+}
